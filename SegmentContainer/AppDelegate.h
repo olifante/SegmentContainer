c40bf28eb81c3ef30d469246ae8f06cedb2119c3
Tiago Henriques <trinosauro@gmail.com>
2012-04-18 21:47:46 +0100

    Initial Commit

---
+++ SegmentContainer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SegmentContainer
+//
+//  Created by Tiago Henriques on 4/18/12.
+//  Copyright (c) 2012 PT Comunicações. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SegmentContainer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SegmentContainer
+//
+//  Created by Tiago Henriques on 4/18/12.
+//  Copyright (c) 2012 PT Comunicações. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
